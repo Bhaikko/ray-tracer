ea65637c21db4f8b7bc33a7e0e45becf0b291d71
Siddharth <siddhartha.pawar17@gmail.com>
2022-04-13 10:22:39 +0530

    Ray Sphere intersection added

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,28 @@
 #include "./../include/color.h"
 #include "./../include/ray.h"
 
+// Ray - Sphere Intersection
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+    // Based on Ray-sphere intersection quadratic equation
+    vec3 oc = r.origin() - center;
+    double a = dot(r.direction(), r.direction());
+    double b = 2.0 * dot(oc, r.direction());
+    double c = dot(oc, oc) - radius * radius;
+
+    double discriminant = b * b - 4 * a * c;
+
+    return (discriminant > 0);
+}
+
 // Lerping ray color between Blue and White based on its y coordinate
 color ray_color(const ray& r)
 {
+    // Checking Sphere intersection
+    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+        return color(1, 0, 0);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     double t = 0.5 * (unit_direction.y() + 1.0);    // Mapping from [-1, 1] -> [0, 1]
 
