7677968b56cbbe655d7401942f182e12c9fd092d
Siddharth <siddhartha.pawar17@gmail.com>
2022-04-13 10:27:56 +0530

    Ray Sphere intersection with Normal visualisation added

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include "./../include/ray.h"
 
 // Ray - Sphere Intersection
-bool hit_sphere(const point3& center, double radius, const ray& r)
+double hit_sphere(const point3& center, double radius, const ray& r)
 {
     // Based on Ray-sphere intersection quadratic equation
     vec3 oc = r.origin() - center;
@@ -15,19 +15,26 @@ bool hit_sphere(const point3& center, double radius, const ray& r)
 
     double discriminant = b * b - 4 * a * c;
 
-    return (discriminant > 0);
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
 // Lerping ray color between Blue and White based on its y coordinate
 color ray_color(const ray& r)
 {
     // Checking Sphere intersection
-    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
-        return color(1, 0, 0);
-    }
+    double t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        // Getting Normal vector from Point of intersection and center of Sphere
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }    
 
     vec3 unit_direction = unit_vector(r.direction());
-    double t = 0.5 * (unit_direction.y() + 1.0);    // Mapping from [-1, 1] -> [0, 1]
+    t = 0.5 * (unit_direction.y() + 1.0);    // Mapping from [-1, 1] -> [0, 1]
 
     // Blending between Blue and White
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
