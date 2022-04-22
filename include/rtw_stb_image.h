aada1ddc33bdb4c423830e617b116fdadca216c9
Siddharth <siddhartha.pawar17@gmail.com>
2022-04-22 10:05:27 +0530

    std_image.h added for texture read

---
+++ include/rtw_stb_image.h
@@ -0,0 +1,17 @@
+// Handles headers for std_image library
+
+#ifndef RTWEEKEND_STB_IMAGE_H
+#define RTWEEKEND_STB_IMAGE_H
+
+#ifdef _MSC_VER
+    #pragma warning (push, 0)
+#endif 
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "./../helper/stb_image.h"
+
+#ifdef _MSC_VER
+    #pragma warning (pop)
+#endif 
+
+#endif
